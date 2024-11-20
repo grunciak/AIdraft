2b1e11882bfed272bf4eed0518c42b023f9243c1
grunciak <57137345+grunciak@users.noreply.github.com>
2024-11-20 14:30:43 +0100

    Update local2.py

--- local2.py
+++ local2.py
@@ -42,7 +42,6 @@ st.write("Zidentyfikowane kolumny alarmów:", alarm_columns)
  # Diagnostyka - sprawdzenie rozkładu klas przed SMOTE
 class_counts = y_train.value_counts()
 st.write("Rozkład klas w y_train:", class_counts)
-
 if alarm_columns:
     # Selectbox for choosing the alarm column
     selected_alarm = st.selectbox('Wybierz kolumnę alarmu', alarm_columns)
